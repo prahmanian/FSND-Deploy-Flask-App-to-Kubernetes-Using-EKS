69bfc97253d47ed438554887575f996b8a718fc1
Pedram Rahmanian <pedram@gutenmade.com>
2020-07-11 01:44:50 -0500

    reset test file to remove intended failure

--- test_main.py
+++ test_main.py
@@ -27,7 +27,7 @@ def test_health(client):
     assert response.status_code == 200
     assert response.json == 'Healthy'
     # testing pipeline by breaking the test
-    assert False
+    # assert False
 
 def test_auth(client):
     body = {'email': EMAIL,
